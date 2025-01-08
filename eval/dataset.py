790c6a7a6484417bf440a2e1d80501d476a6cf1f
Andrea Zenotto s327473 <s327473@studenti.polito.it>
2025-01-08 16:49:55 +0100

    update dataset code

--- eval/dataset.py
+++ eval/dataset.py
@@ -19,7 +19,7 @@ def is_image(filename):
     return any(filename.endswith(ext) for ext in EXTENSIONS)
 
 def is_label(filename):
-    return filename.endswith("_labelTrainIds.png")
+    return filename.endswith("_labelIds.png")
 
 def image_path(root, basename, extension):
     return os.path.join(root, f'{basename}{extension}')
@@ -84,9 +84,9 @@ class cityscapes(Dataset):
 
         #print(filename)
 
-        with open(image_path_city(self.images_root, filename), 'rb') as f:
+        with open(filename, 'rb') as f:
             image = load_image(f).convert('RGB')
-        with open(image_path_city(self.labels_root, filenameGt), 'rb') as f:
+        with open(filenameGt, 'rb') as f:
             label = load_image(f).convert('P')
 
         if self.input_transform is not None:
