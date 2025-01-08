c8b2ea2bf3d7824a917837eb642767e08294e543
Andrea Zenotto s327473 <s327473@studenti.polito.it>
2025-01-08 18:00:06 +0100

    added original datasets

--- eval/evalAnomaly.py
+++ eval/evalAnomaly.py
@@ -125,17 +125,11 @@ def main():
         mask = Image.open(pathGT)
         ood_gts = np.array(mask)
 
-        if "RoadObsticle21" in pathGT:
-            ood_gts = ood_gts[:,:,1]
-            ood_gts = np.where((ood_gts==102), 1, ood_gts)
-            ood_gts = np.where((ood_gts==255), 0, ood_gts)
         if "RoadAnomaly" in pathGT:
             if "RoadAnomaly21" in pathGT:
-                ood_gts = ood_gts[:,:,1]
-                ood_gts = np.where((ood_gts==102), 1, ood_gts)
-                ood_gts = np.where((ood_gts==255), 0, ood_gts)
+                        ood_gts = np.where((ood_gts==255), 1, ood_gts)
             else:
-                ood_gts = np.where((ood_gts==2), 1, ood_gts)
+                        ood_gts = np.where((ood_gts==2), 1, ood_gts)
         if "FS" in pathGT:
             ood_gts = np.where((ood_gts==255), 1, ood_gts)
 
