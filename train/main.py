04e0ade5803931900f9be5e96f88f0f208ce4323
Andrea Zenotto s327473 <s327473@studenti.polito.it>
2025-01-22 20:29:58 +0100

    fc pt22

--- train/main.py
+++ train/main.py
@@ -260,8 +260,8 @@ def train(args, model, enc=False):
                 if args.loss == "eim":
                     loss = criterion(outputs[1], targets[:, 0])
                 else:
-                    outputs
                     loss = criterion(outputs[0], targets[:, 0])
+                outputs = outputs[0]
             else:
                 loss = criterion(outputs, targets[:, 0])
 
@@ -330,11 +330,11 @@ def train(args, model, enc=False):
             if args.model == "bisenet":
                 outputs = outputs[1]
             if args.model == "erfnet":
-                outputs = outputs[0]
                 if args.loss == "eim":
                     loss = criterion(outputs[1], targets[:, 0])
                 else:
                     loss = criterion(outputs[0], targets[:, 0])
+                outputs = outputs[0]
             else:
                 loss = criterion(outputs, targets[:, 0])
             epoch_loss_val.append(loss.item())
